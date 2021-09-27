9b3c9880d3e4adcd0f8b67e71352d7daa0647161
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-09-27 15:23:38 +0300

    update

--- Demo EF Core/Demo EF Core/Program.cs
+++ Demo EF Core/Demo EF Core/Program.cs
@@ -4,9 +4,9 @@ namespace Demo_EF_Core
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
