f564310cbc9373dfa2137adacefedca41b6e76b5
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-10-08 20:17:47 +0300

    update

--- 02. ORM Fundamentals/MiniORM/ChangeTracker.cs
+++ 02. ORM Fundamentals/MiniORM/ChangeTracker.cs
@@ -1,4 +1,19 @@
-﻿namespace MiniORM
+﻿using System.Collections.Generic;
+
+namespace MiniORM
 {
-	// TODO: Create your ChangeTracker class here.
+    internal class ChangeTracker<T> where T : class, new()
+    {
+        private readonly List<T> allEntities;
+        private readonly List<T> added;
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = new 
+        }
+    }
 }
