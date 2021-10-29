c0c9d6d08a3f38caf98fcba237992c573ec4a297
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-10-29 20:06:51 +0300

    update

--- 04. Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
+++ 04. Entity Relations/P01_StudentSystem/Data/StudentSystemContext.cs
@@ -1,7 +1,32 @@
-﻿namespace P01_StudentSystem.Data
-{
-    public class StudentSystemContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+using P01_StudentSystem.Data.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem; Integrated Security=true;");
+            }
+        }
     }
 }
--- 04. Entity Relations/P01_StudentSystem/StartUp.cs
+++ 04. Entity Relations/P01_StudentSystem/StartUp.cs
@@ -1,10 +1,30 @@
-﻿namespace P01_StudentSystem
+﻿using P01_StudentSystem.Data;
+using P01_StudentSystem.Data.Models;
+using System;
+
+namespace P01_StudentSystem
 {
     public class StartUp
     {
         public static void Main()
         {
+            // test insert in database 
 
+            var db = new StudentSystemContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            for (int i = 0; i < 5; i++)
+            {
+                db.Students.Add(new Student
+                {
+                    Name = "Daniel" + i,
+                    PhoneNumber = "033325252" + i,
+                    RegisteredOn = DateTime.UtcNow,
+                    Birthday = new DateTime(1990 + i, 1, 1)
+                });
+            }
+            db.SaveChanges();
         }
     }
 }
