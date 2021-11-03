5020c936454ae5e7d8501abbdc5b9b446144ac8c
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-11-03 17:21:31 +0200

    update

---
+++ 05. LINQ/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using MusicHub.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public virtual Genre Genre { get; set; }
+
+        public int? AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+
+        [Required]
+        public int? WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public decimal Price { get; set; }
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+    }
+}
---
+++ 05. LINQ/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using MusicHub.Data.Models;
+
+namespace MusicHub.Data
+{
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Performer> Performers { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SongPerformer> SongPerformers { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SongPerformer>(x =>
+            {
+                x.HasKey(x => new { x.PerformerId, x.SongId });
+            });
+
+            builder.Entity<SongPerformer>(x =>
+            {
+                x.HasOne(e => e.Song)
+                .WithMany(s => s.SongPerformers)
+                .HasForeignKey(e => e.SongId);
+            });
+
+            builder.Entity<SongPerformer>(x =>
+            {
+                x.HasOne(e => e.Performer)
+                .WithMany(s => s.PerformerSongs)
+                .HasForeignKey(e => e.PerformerId);
+            });
+        }
+    }
+}
