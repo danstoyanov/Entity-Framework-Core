b31e38c2b57086385a9a1a4e2ad73cd9fdaf07c4
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-11-12 21:39:08 +0200

    update

---
+++ 09. XML Processing/ProductShop/ProductShop/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var name = "Daniel";
+
+            Console.WriteLine(name);
+        }
+    }
+}
