cb15c5b6eaaf41b4bf089f7fb9e9aa304e24c4cc
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-11-23 18:59:21 +0200

    update

--- 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlProjectModel.cs
+++ 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlProjectModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using TeisterMask.Data.Models.Enums;
 
 namespace TeisterMask.DataProcessor.ImportDto
 {
     public class XmlProjectModel
     {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
         public string Name { get; set; }
-        public DateTime OpenDate { get; set; }
-        public DateTime DueDate { get; set; }
+
+        [Required]
+        public string OpenDate { get; set; }
+
+        [Required]
+        public string? DueDate { get; set; }
+
         public XmlTaskModel[] Tasks { get; set; }
     }
 }
--- 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlTaskModel.cs
+++ 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlTaskModel.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Xml.Serialization;
+using TeisterMask.Data.Models.Enums;
 
 namespace TeisterMask.DataProcessor.ImportDto
 {
+    [XmlType("")]
     public class XmlTaskModel
     {
         public string Name { get; set; }
         public DateTime OpenDate { get; set; }
         public DateTime DueTime { get; set; }
-        public ExecutionType ExecutionType { get; set; }
-        public LabelType LabelType { get; set; }
+        public ExecutionType? ExecutionType { get; set; }
+        public LabelType? LabelType { get; set; }
     }
 }
