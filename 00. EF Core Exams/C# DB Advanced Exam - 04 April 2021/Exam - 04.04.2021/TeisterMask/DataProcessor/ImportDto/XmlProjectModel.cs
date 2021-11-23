9d47117b7e1ef7d0ef158b6d23751fa5405e16f8
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-11-23 18:48:56 +0200

    update

---
+++ 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlProjectModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class XmlProjectModel
+    {
+        public string Name { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public XmlTaskModel[] Tasks { get; set; }
+    }
+}
---
+++ 00. EF Core Exams/C# DB Advanced Exam - 04 April 2021/Exam - 04.04.2021/TeisterMask/DataProcessor/ImportDto/XmlTaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class XmlTaskModel
+    {
+        public string Name { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime DueTime { get; set; }
+        public ExecutionType ExecutionType { get; set; }
+        public LabelType LabelType { get; set; }
+    }
+}
