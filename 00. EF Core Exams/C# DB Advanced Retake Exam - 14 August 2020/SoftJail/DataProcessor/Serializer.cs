278f214f4b94f7a8ef846679327219b24d4daefd
Daniel Stoyanov <dan.stoyanov@abv.bg>
2021-12-01 13:32:02 +0200

    update

--- 00. EF Core Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
+++ 00. EF Core Exams/C# DB Advanced Retake Exam - 14 August 2020/SoftJail/DataProcessor/Serializer.cs
@@ -1,19 +1,70 @@
 ﻿namespace SoftJail.DataProcessor
 {
-
     using Data;
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Globalization;
+    using System.Xml.Serialization;
+
+    using Newtonsoft.Json;
+
+    using SoftJail.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            return "Not implemented !!!";
+            var jsonPrisonersExport = context
+                .Prisoners
+                .ToArray()
+                .Where(p => ids.Contains(p.Id))
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name,
+                    })
+                    .OrderBy(o => o.OfficerName)
+                    .ToArray(),
+                    TotalOfficerSalary = p.PrisonerOfficers.Sum(o => o.Officer.Salary)
+                })
+                .OrderBy(o => o.Name)
+                .ThenBy(o => o.Id);
+
+            return JsonConvert.SerializeObject(jsonPrisonersExport, Formatting.Indented);
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
         {
-            return "Not implemented !!!";
+
+            var currPrisonerNames = prisonersNames.Split(",").ToArray();
+
+            var xmlPrisonersData = context
+                .Prisoners
+                .Where(p => currPrisonerNames.Contains(p.FullName))
+                .Select(p => new XmlPrisonerExportModel
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    IncarcerationDate = p.IncarcerationDate.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture),
+                    EncryptedMessages = p.Mails.Select(m => new XmlMessageExportModel
+                    {
+                        Description = new string(m.Description.Reverse().ToArray())
+                    })
+                    .ToArray(),
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToList();
+
+            var result = XmlConverter.Serialize(xmlPrisonersData, "Prisoners");
+
+            return result;
         }
     }
 }
